f098bb80b3bafe130332605e8c313860f097387a
MinGeo <markhappy@ajou.ac.kr>
2022-05-22 19:15:45 +0900

    a

--- uthread.c
+++ uthread.c
@@ -312,7 +312,7 @@ void uthread_join(int tid) {
 
 void __exit() {
     /* TODO: You have to implement this function. */
-    // printf("__exit\n");
+    printf("__exit\n");
     struct tcb *temp;
     list_for_each_entry(temp, &tcbs, list) {
         if (temp->tid == MAIN_THREAD_TID && temp->tid == current_tid) {

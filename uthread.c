1e73d7def261836751da9c46f5a6cddec364b068
MinGeo <markhappy@ajou.ac.kr>
2022-05-22 20:51:30 +0900

    a

--- uthread.c
+++ uthread.c
@@ -103,7 +103,7 @@ void next_tcb() {
                         } else {
                             if (p_tcb->tid != n_tcb->tid) {
                                 fprintf(stderr, "SWAP %d -> %d\n", p_tcb->tid, n_tcb->tid);
-                                setcontext(n_tcb->context);
+                                setcontext(t_context);
                             }
                         }
                     }

794c0beaa25e865fe1bd51b0da75f37ea80beada
MinGeo <markhappy@ajou.ac.kr>
2022-05-22 15:09:14 +0900

    a

--- uthread.c
+++ uthread.c
@@ -68,6 +68,7 @@ void next_tcb() {
     {
         case FIFO:
             list_for_each_entry(n_tcb, &tcbs, list) {
+                fprintf(stderr, "LOOP : %d\n", n_tcb->tid);
                 if (n_tcb != NULL && current_tid == n_tcb->tid) {
                     n_tcb = ((struct tcb *)tcbs.next);
                     if ((n_tcb == NULL) || (n_tcb->tid == -1)) {
